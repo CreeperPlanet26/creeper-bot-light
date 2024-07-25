badb2d916b0737143a01a4f8e37d0f718c0ee88b
Creeper <51346607+CreeperPlanet26@users.noreply.github.com>
2024-07-24 21:25:31 -0400

    Update verify.ts

--- src/verify.ts
+++ src/verify.ts
@@ -1,9 +1,9 @@
 import { verifyKey } from "discord-interactions";
 
-export async function verify(req: Request) {
+export async function verify(req: Request, txt: string) {
     const signature = req.headers.get('X-Signature-Ed25519');
     const timestamp = req.headers.get('X-Signature-Timestamp');
-    const isValidRequest = await verifyKey(await req.text(), signature, timestamp, process.env.PUBLIC_KEY);
+    const isValidRequest = await verifyKey(txt, signature, timestamp, process.env.PUBLIC_KEY);
     if (!isValidRequest) {
         return new Response("Bad request signature", { status: 400, })
     }
