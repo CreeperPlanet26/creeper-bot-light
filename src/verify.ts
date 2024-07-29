9db2fdc8a5b2ea9336ffd92f65cc7c16bd0a4446
CreeperPlanet26 <51346607+CreeperPlanet26@users.noreply.github.com>
2024-07-29 12:42:07 -0400

    logs

--- src/verify.ts
+++ src/verify.ts
@@ -1,9 +1,12 @@
 import { verifyKey } from "discord-interactions";
 
 export async function verify(req: Request, txt: string) {
+    console.log("this is the text", txt)
     const signature = req.headers.get('X-Signature-Ed25519');
     const timestamp = req.headers.get('X-Signature-Timestamp');
     const isValidRequest = await verifyKey(txt, signature, timestamp, process.env.PUBLIC_KEY);
+
+    console.log("we were able to await. the request is valid.")
     if (!isValidRequest) {
         return new Response("Bad request signature", { status: 400, })
     }
