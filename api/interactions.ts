2999d37041cea53ee9e54271f42c4ca63ae06edf
CreeperPlanet26 <51346607+CreeperPlanet26@users.noreply.github.com>
2024-07-29 12:08:25 -0400

    try without mongoose

--- api/interactions.ts
+++ api/interactions.ts
@@ -1,4 +1,4 @@
-import "../src/database"
+// import "../src/database"
 import { TEST_SERVER } from "../src";
 import mongoose from "mongoose";
 import { verifyKey, InteractionResponseType, InteractionType, InteractionResponseFlags } from "discord-interactions";
@@ -6,7 +6,6 @@ import { APIInteractionResponse } from "discord.js";
 import { RESTPostAPIInteractionCallbackJSONBody } from "discord-api-types/v10";
 import { verify } from "../src/verify";
 
-process.env.NODE_NO_WARNINGS = 1
 
 export async function GET(req: Request) {
     const signature = req.headers.get('X-Signature-Ed25519');
@@ -23,27 +22,27 @@ export async function GET(req: Request) {
 export async function POST(req: Request) {
     const txt = await req.text()
     await verify(req, txt)
-console.log("body", req.body)
-console.log(req)
-console.log("txt", txt)
+    console.log("body", req.body)
+    console.log(req)
+    console.log("txt", txt)
     const message = JSON.parse(txt) as { type: InteractionType, data: APIInteractionResponse };
-   
-if (message.type === InteractionType.APPLICATION_COMMAND) {
-    console.log("the command is an applicaiton responding", message)
-    console.log("resoonding with type", InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE)
-    return new Response({
-        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-        data: {
-            content: 'Hello world',
-        }
-    })
+
+    if (message.type === InteractionType.APPLICATION_COMMAND) {
+        console.log("the command is an applicaiton responding", message)
+        console.log("resoonding with type", InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE)
+        return new Response(JSON.stringify({
+            type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+            data: {
+                content: 'Hello world',
+            }
+        }))
     }
-    
+
     if (message.type === InteractionType.PING) {
         console.log("ping", message)
-    return new Response(JSON.stringify({
-        type: InteractionResponseType.PONG,
-    }))
+        return new Response(JSON.stringify({
+            type: InteractionResponseType.PONG,
+        }))
     }
 }
 
