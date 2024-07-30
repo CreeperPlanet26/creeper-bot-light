7aec2d01520c60244b638c0ba5222c6124d1388e
Creeper <51346607+CreeperPlanet26@users.noreply.github.com>
2024-07-29 20:43:02 -0400

    Nacl

--- src/verify.ts
+++ src/verify.ts
@@ -1,13 +1,36 @@
 import { verifyKey } from "discord-interactions";
 
+import nacl from "tweetnacl"
+
 export async function verify(req: Request, txt: string) {
     console.log("this is the text", txt)
-    const signature = req.headers.get('X-Signature-Ed25519');
-    const timestamp = req.headers.get('X-Signature-Timestamp');
-    const isValidRequest = await verifyKey(txt, signature, timestamp, process.env.PUBLIC_KEY);
+//    const signature = req.headers.get('X-Signature-Ed25519');
+//    const timestamp = req.headers.get('X-Signature-Timestamp');
+//    const isValidRequest = await verifyKey(txt, signature, timestamp, process.env.PUBLIC_KEY);
 
-    console.log("we were able to await. the request is valid.")
-    if (!isValidRequest) {
-        return new Response("Bad request signature", { status: 400, })
-    }
+//    console.log("we were able to await. the request is valid.")
+//    if (!isValidRequest) {
+ //       return new Response("Bad request signature", { status: 400, })
+ //   }
+
+
+
+
+    
+// Your public key can be found on your application in the Developer Portal
+const PUBLIC_KEY = process.env.PUBLIC_KEY
+
+const signature = req.headers.get("X-Signature-Ed25519");
+const timestamp = req.headers.get("X-Signature-Timestamp");
+// const body = req.rawBody; // rawBody is expected to be a string, not raw bytes
+
+const isVerified = nacl.sign.detached.verify(
+    Buffer.from(timestamp + txt),
+    Buffer.from(signature, "hex"),
+    Buffer.from(PUBLIC_KEY, "hex")
+);
+
+if (!isVerified) 
+     return new Response("Bad request signature", { status: 400, })
+    
 }
