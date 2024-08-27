7c47ffda19d04e21657bbf975ef5af2288fe75d2
CreeperPlanet26 <51346607+CreeperPlanet26@users.noreply.github.com>
2024-08-27 14:56:42 -0400

    defer again

--- src/index.ts
+++ src/index.ts
@@ -54,8 +54,8 @@ export const onInteractionCreate = async (i: Interaction, c?) => {
     // console.log("is this command the same name OR context menu", i.isMessageContextMenuCommand() || i.commandName === replyFetcherCommand.name)
     if (!i.isMessageContextMenuCommand() || i.commandName !== replyFetcherCommand.name) console.log("command is not the one.")
     if (!i.isMessageContextMenuCommand() || i.commandName !== replyFetcherCommand.name) return;
-    // console.log("deferring reply...")
-    // i.deferReply();
+    console.log("deferring reply...")
+    i.deferReply();
 
     // fetch entire channel history for first time. check db here if not already saved. update it otherwise. Fetch after the initial message if there is already data in db. Should not have [message]
     // try to finish installing top chunk of channel if not done already. (oldest in db and keep going up)
