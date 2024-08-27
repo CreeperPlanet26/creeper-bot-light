6f5659376e04b81de7b12d4d8a194e6e6ba1ec4b
CreeperPlanet26 <51346607+CreeperPlanet26@users.noreply.github.com>
2024-08-27 13:43:46 -0400

    revert to 2.5 seconds

--- api/interactions.ts
+++ api/interactions.ts
@@ -31,7 +31,7 @@ export async function POST(req: Request) {
     console.log("txt", txt)
     const message = JSON.parse(txt) as { type: InteractionType, data: APIInteractionResponse };
 
-    await sleep(2700)
+    await sleep(2500)
 
 
     if (message.type === InteractionType.APPLICATION_COMMAND) {
