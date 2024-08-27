2211d3d4f4abd4b56e26d566a31c3ca37d5bce3d
CreeperPlanet26 <51346607+CreeperPlanet26@users.noreply.github.com>
2024-08-27 13:02:00 -0400

    test

--- src/index.ts
+++ src/index.ts
@@ -49,6 +49,7 @@ export const onInteractionCreate = async (i: Interaction) => {
     // console.log("is this command the same name OR context menu", i.isMessageContextMenuCommand() || i.commandName === replyFetcherCommand.name)
     if (!i.isMessageContextMenuCommand() || i.commandName !== replyFetcherCommand.name) console.log("command is not the one.")
     if (!i.isMessageContextMenuCommand() || i.commandName !== replyFetcherCommand.name) return;
+    console.log("deferring reply...")
     i.deferReply();
 
     // fetch entire channel history for first time. check db here if not already saved. update it otherwise. Fetch after the initial message if there is already data in db. Should not have [message]
