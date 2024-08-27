c8c666e0765ab21b345ec8691151de1ab2048aad
CreeperPlanet26 <51346607+CreeperPlanet26@users.noreply.github.com>
2024-08-27 10:55:16 -0400

    rm import

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,6 @@ import { MessagesTable, messagesTable } from "./schema";
 import { and, asc, desc, eq, lt, max, sql } from "drizzle-orm";
 import { Client as PgClient } from "pg";
 import { drizzle } from "drizzle-orm/node-postgres";
-import { before } from "node:test";
 import { performDFS } from "./dfs";
 
 export const TEST_SERVER = "640262033329356822";
@@ -37,112 +36,33 @@ client.on("ready", async () => {
     // console.log((await channel.messages.fetch({ limit: 1 })).first())
 })
 
-client.on("interactionCreate", async (i) => {
+const onInteractionCreate = async (i: Interaction) => {
     STARTED_AT = Date.now();
     console.time('interactionCreate')
     if (!i.isMessageContextMenuCommand() || i.commandName !== replyFetcherCommand.name) return;
     i.deferReply();
-
-    // let timeLimitReached = false;
-    // setTimeout(() => { timeLimitReached = true; }, 2000) //50000
-
-
-    console.log("starting setTimeout")
-    // oldest message in db
-    const [oldestRow] = await db
-        .select({ id: messagesTable.id, timestamp: messagesTable.timestamp })
-        .from(messagesTable)
-        .where(eq(messagesTable.channelId, i.channel.id))
-        .orderBy(asc(messagesTable.timestamp))
-        .limit(1)
-
-    console.log("oldestRow", oldestRow)
-
-
-    await saveNewestToOldest(i)
-    await saveCursorAndAbove(i)
-    await fetchNewestToNewest(i)
-    console.log(await performDFS(i, client))
 
     // fetch entire channel history for first time. check db here if not already saved. update it otherwise. Fetch after the initial message if there is already data in db. Should not have [message]
     // try to finish installing top chunk of channel if not done already. (oldest in db and keep going up)
     // fetch from newest message in channel to the newest message in db.
     // after 55 seconds has passed since invoked time,   
     try {
-        //         const oldMessages = await fetchMessages(oldestRow, timeLimitReached, i)
-
-        //         console.timeEnd('interactionCreate')
 
 
-
-        //         if (oldMessages.length > 0 && timeLimitReached) {
-        //             oldMessages[0].cursor = true;
-        //             await db.insert(messagesTable).values(oldMessages)
-        //             i.channel.send("limit reached while fetching old messages");
-        //             return i.channel.send(`Only installed old ${oldMessages.length} messages between ${oldMessages[oldMessages.length - 1].timestamp.toLocaleString()} and ${oldMessages[0].timestamp.toLocaleString()}. Re-run the command to download the rest.`)
-        //         }
-
-        //         console.log("old messages", oldMessages.length)
-        //         await db.update(messagesTable).set({ cursor: true }).where(and(eq(messagesTable.cursor, true), eq(messagesTable.channelId, i.channel.id)))
-        //         oldMessages.length > 0 && await db.insert(messagesTable).values(oldMessages)
-        //         oldMessages.length > 0 && i.channel.send(`Finished installed old ${oldMessages.length} messages between ${oldMessages[oldMessages.length - 1].timestamp.toLocaleString()} and ${oldMessages[0].timestamp.toLocaleString()}.`)
-
-        //         // then fetch from cursor to up to newest message in db
-        //         const [cursorRow] = await db
-        //             .select({ id: messagesTable.id })
-        //             .from(messagesTable)
-        //             .where(and(eq(messagesTable.channelId, i.channel.id), eq(messagesTable.cursor, true)))
-        //             .limit(1)
-
-        //         if (cursorRow?.id) {
-        //             const [{ id: cursorNewestMessageId }] = await db
-        //                 .select({ id: messagesTable.id })
-        //                 .from(messagesTable)
-        //                 .where(eq(messagesTable.channelId, i.channel.id))
-        //                 .orderBy(desc(messagesTable.timestamp))
-        //                 .limit(1)
-
-        //             const cursorMessages = await fetchMessages(cursorRow, timeLimitReached, i, cursorNewestMessageId);
-        //             if (timeLimitReached) {
-        //                 console.log("time limit reached cursor messages", cursorMessages.length)
-        //                 cursorMessages[0].cursor = true;
-        //                 cursorMessages.length > 0 && await db.insert(messagesTable).values(cursorMessages)
-        //                 i.channel.send("limit reached while fetching cursor messages");
-        //                 return i.channel.send(`Only installed cursor ${cursorMessages.length} messages between ${cursorMessages[cursorMessages.length - 1].timestamp.toLocaleString()} and ${cursorMessages[0].timestamp.toLocaleString()}. Re-run the command to download the rest.`)
-        //             }
-
-        //             await db.update(messagesTable).set({ cursor: false }).where(eq(messagesTable.cursor, true))
-        //             cursorMessages.length > 0 && await db.insert(messagesTable).values(cursorMessages)
-        //             cursorMessages.length > 0 && i.channel.send(`Finished installed cursor ${cursorMessages.length} messages between ${cursorMessages[cursorMessages.length - 1].timestamp.toLocaleString()} and ${cursorMessages[0].timestamp.toLocaleString()}.`)
-        //         }
-
-        //         // then fetch from newest message in channel up to newest message in db
-        //         const [newestRow] = await db
-        //             .select({ id: messagesTable.id })
-        //             .from(messagesTable)
-        //             .where(eq(messagesTable.channelId, i.channel.id))
-        //             .orderBy(desc(messagesTable.timestamp))
-        //             .limit(1)
-
-        //         const newMessages = await fetchMessages(newestRow, timeLimitReached, i, null, true);
-        //         if (newMessages.length > 0 && timeLimitReached) {
-        //             newMessages[0].cursor = true;
-        //             await db.insert(messagesTable).values(newMessages)
-        //             i.channel.send("limit reached while fetching new messages");
-        //             return i.channel.send(`Only installed ${newMessages.length} new messages between ${newMessages[newMessages.length - 1].timestamp.toLocaleString()} and ${newMessages[0].timestamp.toLocaleString()}. Re-run the command to download the rest.`)
-
-        //         }
-        //         console.log("new messages", newMessages.length)
-        //         await db.update(messagesTable).set({ cursor: false }).where(eq(messagesTable.cursor, true))
-        //         newMessages.length > 0 && await db.insert(messagesTable).values(newMessages)
-        //         newMessages.length > 0 && i.channel.send(`Finished installed new ${newMessages.length} messages between ${newMessages[newMessages.length - 1].timestamp.toLocaleString()} and ${newMessages[0].timestamp.toLocaleString()}.`)
-
+        await saveNewestToOldest(i)
+        await saveCursorAndAbove(i)
+        await fetchNewestToNewest(i)
+        console.log(await performDFS(i, client))
 
     } catch (error) {
         console.error(error)
         i.editReply("An error occurred")
     }
-})
+}
+
+process.env.NODE_ENV !== "production" && client.on("interactionCreate", onInteractionCreate)
+
+
 
 // find the oldest row then fetch messages before that.
 async function saveNewestToOldest(i: MessageContextMenuCommandInteraction<CacheType>) {
@@ -400,85 +320,6 @@ async function fetchReferences(i: MessageContextMenuCommandInteraction<CacheType
 
     return msgs
 }
-
-// async function fetchMessages(row, timeLimitReached, i: MessageContextMenuCommandInteraction<CacheType>, cursorNewestMessageId?, fetchTillNewest?) {
-//     console.log("should fetch till newest", fetchTillNewest)
-//     const interval = setInterval(() => {
-//         i.editReply(`Downloading ${messages.length} messages between ${messages[messages.length - 1].timestamp.toLocaleString()} and ${messages[0].timestamp.toLocaleString()}...`)
-//     }, 20000)
-//     let message: Message;
-//     if (fetchTillNewest) message = (await i.channel.messages.fetch({ limit: 1 })).first();
-//     else if (row?.id) message = await i.channel.messages.fetch(row.id)
-//     else message = (await i.channel.messages.fetch({ limit: 1 })).first();
-
-//     console.log("message test", message)
-//     console.log("message first time", message && message.createdAt.toLocaleString())
-//     // if (message === row.id) return []
-
-//     i
-//     let messages = row?.id ? [] : [{
-//         content: message.content,
-//         authorId: message.author.id,
-//         id: message.id,
-//         channelId: message.channelId,
-//         timestamp: message.createdAt,
-//         reference: message.reference?.messageId,
-//         cursor: false
-//     }];
-
-//     while (message && !timeLimitReached) {
-//         console.log(messages.length, "array", message.createdAt.toLocaleString())
-
-//         console.log("going to fetch messages before", message.createdAt.toLocaleString())
-//         const coll = await i.channel.messages.fetch({ limit: 100, before: message.id })
-//         console.log("message given to us", coll.last()?.createdAt.toLocaleString(), coll.size)
-
-//         if (!coll.size) {
-//             // message = null;
-//             clearInterval(interval);
-//             return [];
-//         }
-//         if (coll.last() === cursorNewestMessageId || coll.last() === row?.id) {
-//             // message = null;
-//             clearInterval(interval);
-//             return []
-//         }
-//         if (fetchTillNewest && coll.has(row?.id)) {
-//             for (const [_, m] of coll) {
-//                 console.log(m.createdAt.toLocaleString())
-//                 if (m.id === row.id) {
-//                     console.log("found row id", m.createdAt.toLocaleString())
-//                     console.log(messages.length)
-
-//                     clearInterval(interval);
-//                     return messages
-//                 }
-//                 messages.push({
-//                     content: m.content,
-//                     authorId: m.author.id,
-//                     id: m.id,
-//                     channelId: m.channelId,
-//                     timestamp: m.createdAt,
-//                     reference: m.reference?.messageId,
-//                     cursor: false
-//                 })
-//             }
-//         }
-//         coll.forEach(m => messages.push({
-//             content: m.content,
-//             authorId: m.author.id,
-//             id: m.id,
-//             channelId: m.channelId,
-//             timestamp: m.createdAt,
-//             reference: m.reference?.messageId,
-//             cursor: false
-//         }));
-//         message = coll.size > 0 ? coll.last() : null;
-//     }
-
-//     clearInterval(interval);
-//     return messages;
-// }
 
 
 async function fetchMessageURL(msgs: MessagesTable[], i: Interaction) {
